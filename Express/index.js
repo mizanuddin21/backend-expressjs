2b239972499297acf34c2b10b27bdc5db3ffd2df
Mizanuddin Auliya Iqbal <65990533+mizanuddin21@users.noreply.github.com>
2024-12-15 15:26:15 +0700

    Update index.js

--- Express/index.js
+++ Express/index.js
@@ -8,13 +8,11 @@ const app = express()
 const port = process.env.PORT || 3000
 
 // Import routes
-import userRoute from './routes/users.js'
 import invoiceRoute from './routes/invoice.js'
 import productRoute from './routes/products.js'
 
 // Middleware
 app.use(bodyParser.json())
-app.use('/users', userRoute)
 app.use('/invoice', invoiceRoute)
 app.use('/product', productRoute)
 
